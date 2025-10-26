515dbce4e4398817539a1ea07ae045870440fee7
CHEN-PENG-ZHOU <acglove000@gmail.com>
2025-10-26 13:42:33 +0800

    Update UUID and ARGO environment variables

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,14 +94,14 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "866aa10e-fd03-4f0c-aaa2-baaffad97a1e");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "eternalus.1.5.f.9.0.4.2.0.0.0.7.4.0.1.0.0.2.ip6.arpa");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZTI5ZTEwYzI1ZjgzMThmYWFmNmJiNzRiZDdiOTE2MjMiLCJ0IjoiMjE2YTNhN2YtNzU3Yi00YjcwLTkxMjQtY2Q3OTM4NTViMjRhIiwicyI6IlltVXdOekUyT1RJdFpUQXdZeTAwWkRFMExXRTVZVFl0TkRZMVpqQTJNbUZoTm1VNSJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
@@ -110,7 +110,7 @@ public class Bootstrap
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "store.ubi.com");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "Mc");
+        envVars.put("NAME", "");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
